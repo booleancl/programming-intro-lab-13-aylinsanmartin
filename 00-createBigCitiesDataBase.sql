827778da1fdcf1721113045ef7ae75fc27875977
aylin <a.sanlazo@gmail.com>
2022-10-12 11:01:44 -0300

    lo que me faltaba

---
+++ 00-createBigCitiesDataBase.sql
@@ -0,0 +1,11 @@
+-- this is a comment
+
+/*
+  also a comment
+*/
+
+--warning: this is for study porpuse only. never drop database in production
+
+drop DATABASE IF EXISTS bigcities;
+
+CREATE DATABASE bigcities;
